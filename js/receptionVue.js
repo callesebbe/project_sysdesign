c21a0b61fdba9e11e7a3225378251120f4fff1a5
IvarssonAndreas <Ivarsson.Andreas@hotmail.se>
2017-02-17 11:38:53 +0100

    Store and load guest capacity from local storage works

--- js/receptionVue.js
+++ js/receptionVue.js
@@ -1,13 +1,20 @@
 //Reception Vue object.
-var v = new Vue({
+var reception = new Vue({
   el: '#mainId',
   data: {
     grade: ''
   },
   methods: {
     goToPage: function(url) {
+      localStorage.setItem( "grade", this.grade);     
       window.location= url;
     },
+      
+    loadReception: function() {
+         this.grade = localStorage.getItem("grade"); 
+         document.getElementById(this.grade).value = "X";
+    },
+      
     changeGrade: function(color) {
       document.getElementById('gradingGreen').value = "";
       document.getElementById('gradingYellow').value = "";
@@ -16,7 +23,8 @@ var v = new Vue({
 
       document.getElementById(color).value = "X";
       this.grade = color;
-      //console.log(this.grade);
     }
   }
 })
+
+reception.loadReception();
