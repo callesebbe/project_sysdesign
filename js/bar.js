e5d31334fce05a767cb64539579302077b46bcae
abbehjelm <abbehjelm@gmail.com>
2017-02-19 15:36:32 +0100

    baruppd

--- js/bar.js
+++ js/bar.js
@@ -7,6 +7,7 @@ Vue.component('todo-item', {
   ',
   props: ['title']
 })
+var orderItems = [];
 
 var temp = 0;
 var app = new Vue({
@@ -16,7 +17,8 @@ var app = new Vue({
     show : true,
     count : 1,
     name : '',
-    WB : true,
+    WB : false,
+    ischecked : false,
     todos: [
     ]
   },
@@ -29,24 +31,43 @@ var app = new Vue({
     },
     placeOrder: function(typee) {
       if (temp > 0){
-      this.todos.push(this.count + "x " + this.name)
+      this.todos.push(this.count + "x " + this.name + ": " + this.allergicheck())
     }
+      this.WB = true,
       temp += 1;
       this.count = 1,
       this.show2 = true,
+      this.uncheckallergi(),
+      orderItems = [],
       this.name = typee
     },
     sendOrder: function() {
       this.show2 = false,
       this.count = 1,
-      this.temp = 0
+      this.temp = 0,
+      this.WB = false,
+      this.todos = []
     },
     reviewOrder: function() {
       if (temp > 0){
-      this.todos.push(this.count + "x " + this.name),
+      this.todos.push(this.count + "x " + this.name + ": " + this.allergicheck()),
       temp = 0,
-      this.show2 = false
+      this.show2 = false,
+      this.uncheckallergi(),
+      orderItems = [],
+      this.WB = false
     }
-    }
+  },
+  allergicheck: function () {
+    var orderItems = [].filter.call(document.getElementsByName('order[]'), function(i) {
+      return i.checked;
+    }).map(function(i) {
+      return i.value;
+    });
+    return orderItems
+  },
+  uncheckallergi: function () {
+    this.ischecked = false
+  }
   }
 })
