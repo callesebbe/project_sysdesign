e58cc6d5bd3c16a175b0eec10c4ddd1e9ec8e6c2
IvarssonAndreas <Ivarsson.Andreas@hotmail.se>
2017-03-02 11:47:15 +0100

    OrderQueue, kontorller null värden från localstorage i info

--- js/kitchenVue.js
+++ js/kitchenVue.js
@@ -39,7 +39,6 @@ var app = new Vue({
   },
   methods: {
 
-
     loadOrders: function(){
       if (JSON.parse(localStorage.getItem("ordersInKitchen")) == null) {
       localStorage.setItem("ordersInKitchen", JSON.stringify(this.ordersInKitchen));
@@ -56,44 +55,12 @@ var app = new Vue({
     } ,
 
     goToPage: function(url) {
-      localStorage.setItem( "grade", this.grade);
-      localStorage.setItem( "lastGrading", this.lastGrading);
-
       window.location= url;
     },
-    changeGrade: function(color) {
-      document.getElementById('gradingGreen').value = "";
-      document.getElementById('gradingYellow').value = "";
-      document.getElementById('gradingOrange').value = "";
-      document.getElementById('gradingRed').value = "";
-
-      document.getElementById(color).value = "X";
-      this.grade = color;
-      this.timeSincelastGrading = 0;
-      this.lastGrading = new Date().getTime();
-    },
-
-    updateGrading: function(){
-      var now = new Date().getTime();
-      var difference = now - this.lastGrading;
-      var differenceInSec = Math.round(difference / 1000);
-      if(differenceInSec <60) {
-        this.timeSincelastGrading = differenceInSec+" seconds ago";
-      }else {
-        this.timeSincelastGrading = Math.round(differenceInSec /60)+" minute(s) ago";
-        }
-      },
-
-    load: function() {
-      this.grade = localStorage.getItem("grade");
-      this.lastGrading = localStorage.getItem("lastGrading");
-      document.getElementById(this.grade).value = "X";
-      this.updateGrading();
-    }
   }
 })
 
+
 //window.setInterval(app.saveOrders,1000);
 window.setInterval(app.loadOrders,1000);
-app.load();
 window.setInterval( app.updateGrading, 1000);
